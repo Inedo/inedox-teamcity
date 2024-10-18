93323239c87d98ad4a988372b28ed0b225fb7743
Greg Divis <gdivis@inedo.com>
2024-10-18 13:43:27 -0400

    update to sdk 3.1

--- TeamCity/InedoExtension/Properties/AssemblyInfo.cs
+++ TeamCity/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Contains operations to import artifacts and trigger builds in TeamCity.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("BuildMaster")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2023")]
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
 [assembly: ComVisible(false)]
 
 [assembly: ScriptNamespace("TeamCity")]
